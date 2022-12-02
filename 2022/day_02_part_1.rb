636dd13843b50e8f837a1230ba67a0b5074a3bee
Daniel Lewis <github@mrdaniellewis.co.uk>
2022-12-02 21:05:10 +0000

    cleaner

--- 2022/day_02_part_1.rb
+++ 2022/day_02_part_1.rb
@@ -2,31 +2,16 @@
 
 data = $stdin.read
 
-PLAYS = {
-  "X" => "R",
-  "Y" => "P",
-  "Z" => "S",
-  "A" => "R",
-  "B" => "P",
-  "C" => "S"
-}
-
 SCORES = {
-  %w[R R] => 3 + 1,
-  %w[R P] => 6 + 2,
-  %w[R S] => 0 + 3,
-  %w[P R] => 0 + 1,
-  %w[P P] => 3 + 2,
-  %w[P S] => 6 + 3,
-  %w[S R] => 6 + 1,
-  %w[S P] => 0 + 2,
-  %w[S S] => 3 + 3
+  "A X" => 3 + 1,
+  "A Y" => 6 + 2,
+  "A Z" => 0 + 3,
+  "B X" => 0 + 1,
+  "B Y" => 3 + 2,
+  "B Z" => 6 + 3,
+  "C X" => 6 + 1,
+  "C Y" => 0 + 2,
+  "C Z" => 3 + 3
 }
 
-score = data.split("\n")
-            .map do |line|
-              plays = line.split(" ").map { PLAYS[_1] }
-              SCORES[plays]
-            end
-
-puts score.sum
+puts data.split("\n").map { SCORES[_1] }.sum
--- 2022/day_02_part_2.rb
+++ 2022/day_02_part_2.rb
@@ -2,49 +2,16 @@
 
 data = $stdin.read
 
-PLAYS = {
-  "X" => "R",
-  "Y" => "P",
-  "Z" => "S",
-  "A" => "R",
-  "B" => "P",
-  "C" => "S"
-}
-
-WINS = {
-  "R" => "P",
-  "P" => "S",
-  "S" => "R"
-}
-
-LOSE = {
-  "R" => "S",
-  "P" => "R",
-  "S" => "P"
-}
-
 SCORES = {
-  %w[R R] => 3 + 1,
-  %w[R P] => 6 + 2,
-  %w[R S] => 0 + 3,
-  %w[P R] => 0 + 1,
-  %w[P P] => 3 + 2,
-  %w[P S] => 6 + 3,
-  %w[S R] => 6 + 1,
-  %w[S P] => 0 + 2,
-  %w[S S] => 3 + 3
+  "A X" => 0 + 3, # lose R + S
+  "A Y" => 3 + 1, # draw R + R
+  "A Z" => 6 + 2, # win  R + P
+  "B X" => 0 + 1, # lose P + R
+  "B Y" => 3 + 2, # draw P + P
+  "B Z" => 6 + 3, # win  P + S
+  "C X" => 0 + 2, # lose S + P
+  "C Y" => 3 + 3, # draw S + S
+  "C Z" => 6 + 1  # win  S + R
 }
 
-score = data.split("\n")
-            .map do |line|
-              elf, play = line.split(" ")
-              elf = PLAYS[elf]
-              you = case play
-                    when "Y" then elf
-                    when "X" then LOSE[elf]
-                    when "Z" then WINS[elf]
-                    end
-              SCORES[[elf, you]]
-            end
-
-puts score.sum
+puts data.split("\n").map { SCORES[_1] }.sum
