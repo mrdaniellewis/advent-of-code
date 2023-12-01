f06aa40c77cefd9c60d8590362912a633f9e19d8
Daniel Lewis <github@mrdaniellewis.co.uk>
2023-12-01 16:50:58 +0000

    2020 day 13

--- 2020/day_13_part_1.rb
+++ 2020/day_13_part_1.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
+data = $stdin.read
+
 earliest = nil
 buses = []
 
-DATA.each_line do |line|
-  earliest = line.strip.to_i unless earliest
-  buses = line.strip.split(",").reject { |n| n == "x" }.map(&:to_i) if earliest
+data.each_line do |line|
+  earliest ||= line.strip.to_i
+  buses = line.strip.split(",").reject { _1 == "x" }.map(&:to_i) if earliest
 end
 
 times = buses.map do |number|
@@ -19,7 +21,3 @@ end
 
 time, index = times.each_with_index.min
 puts (time - earliest) * buses[index]
-
-__END__
-1000053
-19,x,x,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,523,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,13,x,x,x,x,23,x,x,x,x,x,29,x,547,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,17
--- 2020/day_13_part_2.rb
+++ 2020/day_13_part_2.rb
@@ -1,88 +1,56 @@
 # frozen_string_literal: true
 
-require "pp"
+data = $stdin.read
 
-buses = nil
+# [number: int, offset: int]
+buses = data
+  .split(/\n/)[1]
+  .strip
+  .split(",")
+  .map { _1 == "x" ? nil : _1.to_i }
+  .map
+  .with_index { [_1, _2] }
+  .reject { _1[0].nil? }
 
-DATA.each_line do |line|
-  buses ||= line.strip.split(",").map { |n| n == "x" ? nil : n.to_i }
-end
+# Largest bus first
+ordered_buses = buses.sort_by { _1[0] }.reverse
+time = 0
+# We know the answer must be a multiple of the first bus, which is never blank
+increment = buses[0][0]
 
-interval = buses[0]
-buses_with_index = buses.each_with_index.to_a.reject { |(value, index)| value.nil? }
+bus_count = 1
 
-results = {}
-buses_with_index.each do |(bus, index)|
-  comp = {}
-
-  buses_with_index.each do |(b, i)|
-    count = 0
-    offset = nil
-    diff = index - i
-
+catch(:done) do
+  loop do
+    last = nil
+    # Start with just one bus, and find where there is a solution
+    # Find the difference between the time of the last solution
+    # As the buses are all primes the increment will be a constant with an initial offset
+    # When we have an increment add a new bus
     loop do
-      match = count + diff >= 0 && (count + diff) % bus == 0
+      # We might be able to exit early
+      throw :done if buses.all? { |(bus, i)| (time + i) % bus == 0 }
 
-      break if offset && match
-      offset ||= count if match
-      count += b
+      if ordered_buses.take(bus_count).all? { |(bus, i)| (time + i) % bus == 0 }
+        break increment = time - last unless last.nil?
+
+        last = time
+      end
+
+      time += increment
     end
 
-    comp[b] = [offset + diff, count - offset]
+    # Add in a bus
+    bus_count += 1
+    # Until we've run out of buses
+    break if bus_count >= buses.length
   end
 
-  results[bus] = comp
+  loop do
+    throw :done if buses.all? { |(bus, i)| (time + i) % bus == 0 }
+
+    time += increment
+  end
 end
 
-
-pp results
-
-# puts matches.reduce(:*)
-
-
-# count = 0
-#
-# loop do
-#   count += max
-#
-#   break if buses_with_index.all? do |(bus, i)|
-#     (count - index + i) % bus == 0
-#   end
-# end
-
-# puts count - index
-
-# times = buses.map do |number|
-#   arrival = 0
-#   loop do
-#     arrival += number
-#     break if arrival >= earliest
-#   end
-#   arrival
-# end
-#
-# time, index = times.each_with_index.min
-# puts (time - earliest) * buses[index]
-
-
-#    0         1         2         3         4         5         6         7         8         9         10        12        13        14        15        16
-#    0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
-# 3  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x  x
-# 7  x      x      x      x      x      x      x      x      x      x      x      x      x      x      x      x      x      x      x      x      x      x      x
-# 11 x          x          x          x          x          x          x          x          x          x          x          x          x          x          x
-# 2  x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x
-#
-# T = 7a       7a = T
-# T + 1 = 13b  77 + 91b = T
-# T + 4 = 59c  350 + 413b = T
-# T + 6 = 31d  56 + 217d = T
-# T + 7 = 19e  126 + 133e = T
-#
-# 7a = 7a
-# 78 + 91b = 13b
-# 354 + 413b = 59c
-# 72 + 217d  = 31d
-# 133 + 133e = 19e
-
-__END__
-7,13,x,x,59,x,31,19
+pp time
