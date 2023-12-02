6d58a98ed30f19dc4ca9a8088df5f03dfcee5300
Daniel Lewis <github@mrdaniellewis.co.uk>
2023-12-02 11:28:13 +0000

    2023 day 2

---
+++ 2023/day_02_part_1.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+data = $stdin.read
+
+games = data.split("\n").to_h do |line|
+  num, plays = /^Game (\d+): (.*)$/.match(line).captures
+  plays = plays.split("; ").map do |draws|
+    draws.split(", ").to_h do |draw|
+      count, colour = /^(\d+) (.*)$/.match(draw).captures
+      [colour.to_sym, count.to_i]
+    end
+  end
+  [num.to_i, plays]
+end
+
+BAG = {
+  red: 12,
+  green: 13,
+  blue: 14
+}
+
+games
+  .select do |_num, plays|
+    plays.all? do |play|
+      BAG.all? do |(k, v)|
+        (play[k] || 0) <= v
+      end
+    end
+  end
+  .keys
+  .sum
+  .then { puts _1 }
---
+++ 2023/day_02_part_2.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+data = $stdin.read
+
+games = data.split("\n").to_h do |line|
+  num, plays = /^Game (\d+): (.*)$/.match(line).captures
+  plays = plays.split("; ").map do |draws|
+    draws.split(", ").to_h do |draw|
+      count, colour = /^(\d+) (.*)$/.match(draw).captures
+      [colour.to_sym, count.to_i]
+    end
+  end
+  [num.to_i, plays]
+end
+
+games
+  .values
+  .map do |plays|
+    bag = { red: 0, green: 0, blue: 0 }
+    plays.flatten.each do |draws|
+      draws.each do |(k, v)|
+        bag[k] = v if bag[k] < v
+      end
+    end
+    bag
+  end
+  .map { _1.values.reduce(:*) }
+  .sum
+  .then { puts _1 }
