ee0d0882acbdcee668aabf7df7d44617786831c3
Daniel Lewis <github@mrdaniellewis.co.uk>
2023-12-06 00:28:47 +0000

    wip

---
+++ 2023/day_05_part_1.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "debug"
+
+data = ARGF.read
+
+seeds = nil
+maps = []
+current_map = {}
+
+data.split("\n").each do |line|
+  case /^(?:(\w+)-to-(\w+) )?(?:(map|seeds): ?)?((?:\d+ ?)+)?$/.match(line).captures
+  in [_, _, "seeds", numbers]
+    seeds = numbers.split(" ").map(&:to_i)
+  in [_, _, "map", nil]
+    maps << current_map
+  in [nil, nil, nil, nil]
+    current_map = {}
+  in [nil, nil, nil, numbers]
+    destination, source, length = numbers.split(" ").map(&:to_i)
+    current_map[Range.new(source, source + length)] = Range.new(destination, destination + length)
+  end
+end
+
+seeds
+  .map do |seed|
+    maps.reduce(seed) do |current, type_maps|
+      source, destination = type_maps.find { |(start)| start.include?(current) }
+      next current unless source
+
+      destination.begin + (current - source.begin)
+    end
+  end
+  .min
+  .then { puts _1 }
