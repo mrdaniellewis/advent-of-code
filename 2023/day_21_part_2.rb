c7e6c96ed960bddee00c2ba3378a491a00ce2c61
Daniel Lewis <github@mrdaniellewis.co.uk>
2023-12-23 10:51:40 +0000

    wip

--- 2023/day_21_part_2.rb
+++ 2023/day_21_part_2.rb
@@ -54,7 +54,55 @@ counts = {
   odd: []
 }
 
-until r == 100
+SIZE = XRANGE.max + 1
+
+distances = Hash.new { Float::INFINITY }
+distances[start] = 0 if count.even?
+heads = [[start, 0]]
+loop do
+  heads = heads.flat_map do |(head, distance)|
+    distance += 1
+    next [] if distance > count
+
+    DIRECTIONS
+      .map { head + _1 }
+      .select { GRID.include?(V[_1[0] % (XRANGE.max + 1), _1[1] % (YRANGE.max + 1)]) }
+      .select do |pos|
+        if distances[pos] > distance
+          distances[pos] = distance if (distance.even? && count.even?) || (distance.odd? && count.odd?)
+          true
+        else
+          false
+        end
+      end
+      .map { [_1, distance] }
+  end
+
+  puts distances.values.tally
+
+  break if heads.empty?
+end
+
+puts debug(distances.map.select { _2 <= count }.to_h)
+
+exit
+distances.map.select do |(pos)|
+  XRANGE.include?(pos[0]) && YRANGE.include?(pos[1])
+end.select { _2 <= count }.then { pp _1.size }
+
+[1, 2].each do |t|
+  (DIRECTIONS + DIALOGONAL_DIRECTIONS).each do |dir|
+    c = distances.map.select do |(pos)|
+      pos -= (dir * SIZE * t)
+      XRANGE.include?(pos[0]) && YRANGE.include?(pos[1])
+    end.select { _2 <= count }.size
+    pp [dir, t, c]
+  end
+end
+
+exit
+
+loop do
   distances = Hash.new { Float::INFINITY }
   distances[start] = 0 if r.even?
   heads = [[start, 0]]
@@ -87,7 +135,7 @@ until r == 100
   full = [(count - XRANGE.max - 1) / (XRANGE.max + 1), 0].max
   puts "full=#{full}"
   puts "full_count=#{(full + 1)**2 + 1}"
-  puts debug(distances.map.select { _2 <= count }.to_h)
+  # puts debug(distances.map.select { _2 <= count }.to_h)
   # pp [r, count, (r + 1)**2, (r + 1)**2 - count]
   diffs << (r + 1)**2 - count
 
@@ -113,7 +161,10 @@ until r == 100
     end
   end
 
-  break if full == 4
+  if r >= SIZE * 3
+    puts debug(distances.map.select { _2 <= count }.to_h)
+    break
+  end
 
   r += 1
 end
@@ -153,6 +204,24 @@ pp distances.map.select { _2 <= count }.size
   # puts debug(ring)
 end
 
-# center
-# + (r - len * 2 - len / 2) ** 2 even and odd
+# x
+# + 4y
+# + 8x
+# + 8x
 #
+# 0  1
+# 2  8
+# 4  16
+# 6  24
+# 8  32
+# 10 40
+#
+#
+# 1 + 2*4 + 4*4 + 6*4 + 8*4
+
+total = 1
+cursor = 2
+while x < target
+  total += x * 4
+  cursor += 2
+end
